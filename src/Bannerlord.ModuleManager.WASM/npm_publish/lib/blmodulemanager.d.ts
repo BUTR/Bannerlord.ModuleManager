fcb8919aed109bb56c1f2cedc3f53bcbef60aa1d
Aragas <personal@aragas.org>
2021-11-25 13:25:21 +0300

    Added new types to raw

--- src/Bannerlord.ModuleManager.WASM/npm_publish/lib/blmodulemanager.d.ts
+++ src/Bannerlord.ModuleManager.WASM/npm_publish/lib/blmodulemanager.d.ts
@@ -1,13 +1,8 @@
- declare module '@butr/blmodulemanager-raw' {
-  export function boot(): Promise<void>;
+import { DotNetRuntime } from "@butr/dotnet-runtime-ts/lib/types/dotnet"
+import { DotNetWasmWrapper } from "@butr/dotnet-runtime-ts/lib/types/wrapper";
 
-  export function invoke(method: string, ...args: unknown[]): void;
-  export function invoke<TType>(method: string, ...array: unknown[]): TType;
-  export function invokeAsync<TType>(method: string, ...array: unknown[]): Promise<TType>;
-  export function invokeAsync(method: string, ...args: unknown[]): Promise<void>;
-
-  export function invokeAssembly(assembly: string, method: string, ...args: unknown[]): void;
-  export function invokeAssembly<TType>(assembly: string, method: string, ...args: unknown[]): TType;
-  export function invokeAssemblyAsync(assembly: string, method: string, ...args: unknown[]): Promise<void>;
-  export function invokeAssemblyAsync<TType>(assembly: string, method: string, ...args: unknown[]): Promise<TType>;
+declare const wrapper: DotNetWasmWrapper;
+declare module '@butr/blmodulemanager-raw' {
+  export const dotnet: DotNetRuntime;
+  export default wrapper;
 }
