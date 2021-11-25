5741e594adfa3140bde33b854490a459316f6c9f
Aragas <personal@aragas.org>
2021-11-25 15:00:31 +0300

    Test

--- src/Bannerlord.ModuleManager.WASM/npm_publish/lib/blmodulemanager.d.ts
+++ src/Bannerlord.ModuleManager.WASM/npm_publish/lib/blmodulemanager.d.ts
@@ -1,8 +1,8 @@
-import { DotNetRuntime } from "@butr/dotnet-runtime-ts/lib/types/dotnet"
-import { DotNetWasmWrapper } from "@butr/dotnet-runtime-ts/lib/types/wrapper";
-
-declare const wrapper: DotNetWasmWrapper;
 declare module '@butr/blmodulemanager-raw' {
+  import { DotNetRuntime } from "@butr/dotnet-runtime-ts/lib/types/dotnet";
+  import { DotNetWasmWrapper } from "@butr/dotnet-runtime-ts/lib/types/wrapper";
+
   export const dotnet: DotNetRuntime;
+  export const wrapper: DotNetWasmWrapper;
   export default wrapper;
 }
