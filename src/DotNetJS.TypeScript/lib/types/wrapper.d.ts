2d5684ec2d79fd9728cc0b8fec9a92b8b3ab5a55
Aragas <personal@aragas.org>
2021-11-27 13:04:31 +0300

    Still no idea what I'm doing

--- src/DotNetJS.TypeScript/lib/types/wrapper.d.ts
+++ src/DotNetJS.TypeScript/lib/types/wrapper.d.ts
@@ -1,11 +1,20 @@
-import { invoke as invokeAssembly, invokeAsync as invokeAssemblyAsync } from "./interop";
+import { getBootStatus, terminate } from "./boot";
+import { BootStatus } from "./data/BootStatus";
+import { createObjectReference, createStreamReference, disposeObjectReference, invoke as invokeAssembly, invokeAsync as invokeAssemblyAsync } from "./interop";
 
 declare function boot(): Promise<void>;
 declare function invoke<TType>(method: string, ...args: any[]): TType;
 declare function invokeAsync<TType>(method: string, ...args: any[]): Promise<TType>;
 
 export interface DotNetWasmWrapper {
+  BootStatus: typeof BootStatus;
+  getBootStatus: typeof getBootStatus;
   boot: typeof boot;
+  terminate: typeof terminate;
+
+  createObjectReference: typeof createObjectReference;
+  disposeObjectReference: typeof disposeObjectReference;
+  createStreamReference: typeof createStreamReference;
 
   invoke: typeof invoke;
   invokeAsync: typeof invokeAsync;
@@ -14,5 +23,18 @@ export interface DotNetWasmWrapper {
   invokeAssemblyAsync: typeof invokeAssemblyAsync;
 }
 
-export declare const wrapper: DotNetWasmWrapper;
-export { boot, invoke, invokeAsync, invokeAssembly, invokeAssemblyAsync };
+declare const wrapper: DotNetWasmWrapper;
+export { 
+  BootStatus,
+  getBootStatus,
+  boot,
+  terminate,
+  createObjectReference,
+  disposeObjectReference,
+  createStreamReference,
+  invoke,
+  invokeAsync,
+  invokeAssembly,
+  invokeAssemblyAsync
+};
+export default wrapper;
