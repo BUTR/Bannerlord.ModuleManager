601e7d975115997a6ba54d1b41bc3702341fa668
Aragas <personal@aragas.org>
2022-01-13 10:15:29 +0300

    Unmerged patterns, that's not supported

--- src/Bannerlord.ModuleManager/ModuleInfoExtended.cs
+++ src/Bannerlord.ModuleManager/ModuleInfoExtended.cs
@@ -48,7 +48,7 @@ namespace Bannerlord.ModuleManager
                 if (dependentModulesList[i]?.Attributes?["Id"] is { } idAttr)
                 {
                     ApplicationVersion.TryParse(dependentModulesList[i]?.Attributes?["DependentVersion"]?.InnerText, out var dVersion);
-                    var isOptional = true;// dependentModulesList[i]?.Attributes?["Optional"] is { InnerText: "true" };
+                    var isOptional = dependentModulesList[i]?.Attributes?["Optional"] is { } optional && optional.InnerText.Equals("true");
                     dependentModules[i] = new DependentModule(idAttr.InnerText, dVersion, isOptional);
                 }
             }
